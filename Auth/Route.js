128100a85ffb82c40e2c2cedd19e6bbb71fd8b8b
Rodrigo <69533120+DevRodrgo@users.noreply.github.com>
2022-08-23 22:46:35 -0300

    Add files via upload

---
+++ Auth/Route.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const router = express.Router()
+const { register } = require("./auth")
+router.route("/register").post(register)
+module.exports = router
+
+const express = require("express");
+const router = express.Router();
+const { register, login } = require("./auth");
+
+router.route("/login").post(login);
+module.exports = router;
+
+const { register, login, update } = require("./auth");
+router.route("/update").put(update);
+
+const { register, login, update, deleteUser } = require("./auth");
+router.route("/deleteUser").delete(deleteUser);
